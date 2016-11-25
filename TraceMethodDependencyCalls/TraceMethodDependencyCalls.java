7b9431118f32017d344fa9fa9d3a074af3ffb179
ks489 <ks489@student.le.ac.uk>
2016-11-25 15:02:42 +0000

    Added some changes to the aspects

--- TraceMethodDependencyCalls/TraceMethodDependencyCalls.java
+++ TraceMethodDependencyCalls/TraceMethodDependencyCalls.java
@@ -7,63 +7,51 @@ import org.aspectj.lang.Signature;
 
 aspect Trace{
 
-        private static Logger logger = Logger.getLogger("tmdcTrace1");
-		private Stack globalMethods;
+	private static Logger logger = Logger.getLogger("tmdcTrace1");
+	private Stack globalMethods;
+	
+	public Trace(){
+	
+	try{
+		globalMethods = new Stack();
+		FileHandler handler=new FileHandler("tmdcTrace1.log",false);
+
+		logger.addHandler(handler);
+
+		handler.setFormatter(new Formatter(){
+
+		public String format(LogRecord record){
+				return record.getMessage()+"\n";
+				}
+				});
+	}
+	catch(Exception e){}
+		System.out.println("Quickly call Trump to build a wall around this error");
+	}
+
+	pointcut callMethods() : (call(* org.apache.commons..*(..))&& !cflow(within(Trace)));
+
+	before(): traceMethods(){
 		
-        public Trace(){
+		Signature sig = thisJoinPointStaticPart.getSignature();				
+		String line =""+ thisJoinPointStaticPart.getSourceLocation().getLine();
+		String sourceName = thisJoinPointStaticPart.getSourceLocation().getWithinType().getCanonicalName();
 		
-        try{
-				globalMethods = new Stack();
-                FileHandler handler=new FileHandler("tmdcTrace1.log",false);
-
-                logger.addHandler(handler);
-
-                handler.setFormatter(new Formatter(){
-
-                        public String format(LogRecord record){
-                                return record.getMessage()+"\n";
-                                }
-                                });
-                        }
-
-        catch(Exception e){}
-			System.out.println("Quickly call Trump to build a wall around this error");
-        }
-
-        pointcut traceMethods() : (call(* *(..))&& !cflow(within(Trace)));
-
-        before(): traceMethods(){
+		if(!globalMethods.empty()){
 			
-			if(!globalMethods.empty()){
-				//(a) log the method call if the current stack is not empty
-			}
-                Signature sig = thisJoinPointStaticPart.getSignature();
-                String line =""+ thisJoinPointStaticPart.getSourceLocation().getLine();
-                String sourceName = thisJoinPointStaticPart.getSourceLocation().getWithinType().getCanonicalName();
-                Logger.getLogger("tmdcTrace1").log(
-                        Level.INFO,
-                        "Call -> "
-                        +  sourceName
-                        +" line " +
-                        line
-                        +" to " +sig.getDeclaringTypeName() + "." + sig.getName()
-                );
-        }
-		
-		after(): traceMethods(){
-				endTime = System.nanoTime();
-                Signature sig = thisJoinPointStaticPart.getSignature();
-                String line =""+ thisJoinPointStaticPart.getSourceLocation().getLine();
-                String sourceName = thisJoinPointStaticPart.getSourceLocation().getWithinType().getCanonicalName();
-                Logger.getLogger("tmteTrace1").log(
-                        Level.INFO,
-                        //"Time -> " + (endTime - startTime) + " Nano Seconds -> Call from "
-						"Execution -> "
-                        +  sourceName
-                        +" line " +
-                        line
-                        +" to " +sig.getDeclaringTypeName() + "." + sig.getName() 
-                );
-				
-        }
+			Logger.getLogger("tmdcTrace1").log(Level.INFO,
+					"Call -> "
+					+  sourceName
+					+" line " +
+					line
+					+" to " +sig.getDeclaringTypeName() + "." + sig.getName()
+			);				
+		}
+		globalMethods.push(sig);
+	}
+	
+	after(): traceMethods(){
+		globalMethods.pop();
+			
+	}
 }
--- TraceMethodTimeExecution/TraceMethodTimeExecution.java
+++ TraceMethodTimeExecution/TraceMethodTimeExecution.java
@@ -6,55 +6,51 @@ import org.aspectj.lang.Signature;
 
 aspect Trace{
 
-        private static Logger logger = Logger.getLogger("tmteTrace1");
-		long startTime = 0L;
-		long endTime = 0L;
-		
-		Timer timerAfer;
-        public Trace(){
+	private static Logger logger = Logger.getLogger("tmteTrace1");
+	long startTime = 0L;
+	long endTime = 0L;
+	
+	Timer timerAfer;
+	public Trace(){
 
-        try{
-                FileHandler handler=new FileHandler("tmteTrace1.log",false);
+	try{
+		FileHandler handler=new FileHandler("tmteTrace1.log",false);
 
-                logger.addHandler(handler);
+		logger.addHandler(handler);
 
-                handler.setFormatter(new Formatter(){
+		handler.setFormatter(new Formatter(){
 
-                        public String format(LogRecord record){
-                                return record.getMessage()+"\n";
-                                }
-                                });
-                        }
+				public String format(LogRecord record){
+						return record.getMessage()+"\n";
+						}
+						});
+	}
+	catch(Exception e){}
 
-        catch(Exception e){}
+	}
 
-        }
-
-        //pointcut traceMethods() : (execution(* *(..))&& !cflow(within(Trace)));
-		//pointcut traceMethods() : (execution(* org.apache.commons.net.ftp.*.*(..))&& !cflow(within(Trace)));
-		pointcut traceMethods() : (execution(* org.apache.commons.net..*(..))&& !cflow(within(Trace)));
-		
-		
-
-        before(): traceMethods(){
-				startTime = System.nanoTime();
-                
-        }
-		
-		after(): traceMethods(){
-				endTime = System.nanoTime();
-                Signature sig = thisJoinPointStaticPart.getSignature();
-                String line =""+ thisJoinPointStaticPart.getSourceLocation().getLine();
-                String sourceName = thisJoinPointStaticPart.getSourceLocation().getWithinType().getCanonicalName();
-                Logger.getLogger("tmteTrace1").log(
-                        Level.INFO,
-                        //"Time -> " + (endTime - startTime) + " Nano Seconds -> Call from "
-						"Execution -> "
-                        +  sourceName
-                        +" line " +
-                        line
-                        +" to " +sig.getDeclaringTypeName() + "." + sig.getName() 
-                );
-				
-        }
+	//pointcut traceMethods() : (execution(* *(..))&& !cflow(within(Trace)));
+	//pointcut traceMethods() : (execution(* org.apache.commons.net.ftp.*.*(..))&& !cflow(within(Trace)));
+	pointcut traceMethods() : (execution(* org.apache.commons.net..*(..))&& !cflow(within(Trace)));
+	
+	before(): traceMethods(){
+		startTime = System.nanoTime();
+			
+	}
+	
+	after(): traceMethods(){
+		endTime = System.nanoTime();
+		Signature sig = thisJoinPointStaticPart.getSignature();
+		String line =""+ thisJoinPointStaticPart.getSourceLocation().getLine();
+		String sourceName = thisJoinPointStaticPart.getSourceLocation().getWithinType().getCanonicalName();
+		Logger.getLogger("tmteTrace1").log(
+				Level.INFO,
+				//"Time -> " + (endTime - startTime) + " Nano Seconds -> Call from "
+				"Execution -> "
+				+  sourceName
+				+" line " +
+				line
+				+" to " +sig.getDeclaringTypeName() + "." + sig.getName() 
+		);			
+	}
 }
